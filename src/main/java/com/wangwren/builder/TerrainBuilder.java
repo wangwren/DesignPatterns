747706b349742becdf8cd99ce17f57839d69f806
wangwren <291762380@qq.com>
2019-06-07 22:54:02 +0800

    Builder设计模式

---
+++ src/main/java/com/wangwren/builder/TerrainBuilder.java
@@ -0,0 +1,8 @@
+package com.wangwren.builder;
+
+public interface TerrainBuilder {
+    TerrainBuilder buildWall();
+    TerrainBuilder buildFort();
+    TerrainBuilder buildMine();
+    Terrain build();
+}
