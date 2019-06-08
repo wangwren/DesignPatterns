5e9fe86f03507a171178ae7aa8d2c3ccf77ccae7
wangwren <291762380@qq.com>
2019-06-08 12:26:32 +0800

    state

---
+++ src/main/java/com/wangwren/state/thread/Action.java
@@ -0,0 +1,5 @@
+package com.wangwren.state.thread;
+
+public class Action {
+    String msg;
+}
---
+++ src/main/java/com/wangwren/state/thread/ThreadState_.java
@@ -0,0 +1,6 @@
+package com.wangwren.state.thread;
+
+public abstract class ThreadState_ {
+    abstract void move(Action input);
+    abstract void run();
+}
